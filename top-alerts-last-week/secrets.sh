c5603e72c4edc6c31cd082a167aac41ed4cd0a88
jacek-pankracek <86568921+jacek-pankracek@users.noreply.github.com>
2025-11-16 23:31:35 +0100

    Remove Zabbix credentials from secrets.sh
    Clear sensitive Zabbix credentials in secrets.sh

--- top-alerts-last-week/secrets.sh
+++ top-alerts-last-week/secrets.sh
@@ -1,3 +1,3 @@
-export ZABBIX_URL="https://management.tadzio.lan/zabbix/"
-export ZABBIX_USER="jacek"
-export ZABBIX_TOKEN="edcfbf0917e9f5480e7e6e3486f19274aea5c9d79ef60fc752dd7775f8b27d1c"
+export ZABBIX_URL=""
+export ZABBIX_USER=""
+export ZABBIX_TOKEN=""
